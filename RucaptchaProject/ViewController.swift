ca4c5dbd38e96aadc335094c42e62b344c1d3d2e
evgen z <evgen-2208@ya.ru>
2022-11-20 12:32:54 +0400

    Initial Commit

---
+++ RucaptchaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RucaptchaProject
+//
+//  Created by Evgen on 20.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
